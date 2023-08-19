b2188e0b7b13060dc3a6ff854ab4e8c1067771f8
Devin Slate <sl8t3@hotmail.com>
2023-08-19 12:18:17 -0600

    refactor to class

--- app.py
+++ app.py
@@ -15,30 +15,51 @@ from langchain.memory import ConversationSummaryMemory, ChatMessageHistory
 TEMPERATURE = 0.5
 MODEL = "gpt-3.5-turbo"
 
-def chat(user_input):
-    prompt = ChatPromptTemplate.from_messages(
-        [
-            SystemMessagePromptTemplate.from_template(
-                f"This is a conversation between a human and an AI philosopher of Stoicism. "
-                "You help the human reflect on the core concepts of Stoicism as though you are Epictetus himself. "
-            ),
-            MessagesPlaceholder(variable_name="history"),
-            HumanMessagePromptTemplate.from_template("{input}"),
-        ]
-    )
 
-    llm = ChatOpenAI(temperature=TEMPERATURE, model=MODEL)
-    memory = ConversationSummaryMemory(llm=OpenAI(), return_messages=True)
-    conversation = ConversationChain(
-        memory=memory, prompt=prompt, llm=llm, verbose=True
-    )
+class StoicChat:
+    def __init__(self):
+        # Initialization of constants
+        self.TEMPERATURE = 0.5
+        self.MODEL = "gpt-3.5-turbo"
 
-    return conversation.predict(input=user_input)
+        # Load or initialize the conversation
+        self.conversation = self._get_or_create_conversation()
+
+    def _get_or_create_conversation(self):
+        """Private method to create or retrieve an existing conversation."""
+        if "conversation" not in st.session_state:
+            prompt = ChatPromptTemplate.from_messages(
+                [
+                    SystemMessagePromptTemplate.from_template(
+                        f"This is a conversation between a human and an AI philosopher of Stoicism. "
+                        "You help the human reflect on the core concepts of Stoicism as though you are Epictetus himself."
+                    ),
+                    MessagesPlaceholder(variable_name="history"),
+                    HumanMessagePromptTemplate.from_template("{input}"),
+                ]
+            )
+
+            llm = ChatOpenAI(temperature=self.TEMPERATURE, model=self.MODEL)
+            memory = ConversationSummaryMemory(llm=OpenAI(), return_messages=True)
+
+            st.session_state.conversation = ConversationChain(
+                memory=memory, prompt=prompt, llm=llm, verbose=True
+            )
+
+        return st.session_state.conversation
+
+    def get_response(self, user_input):
+        """Public method to get a response based on user input."""
+        return self.conversation.predict(input=user_input)
 
 
 def main():
-    st.title('Stoic Reflection with AI')
-    st.write("Welcome to the Stoic reflection tool based on the principles in Epictetus's Enchiridion.")
+    st.title("Stoic Reflection with AI")
+    st.write(
+        "Welcome to the Stoic reflection tool based on the principles in Epictetus's Enchiridion."
+    )
+
+    chat_instance = StoicChat()
 
     if "messages" not in st.session_state:
         st.session_state.messages = []
@@ -50,11 +71,14 @@ def main():
     if prompt := st.chat_input("What is up?"):
         st.chat_message("user").markdown(prompt)
         st.session_state.messages.append({"role": "user", "content": prompt})
-        response = chat(prompt)
+
+        response = chat_instance.get_response(prompt)
+
         with st.chat_message("assistant"):
             st.markdown(response)
 
         st.session_state.messages.append({"role": "assistant", "content": response})
+
 
 if __name__ == "__main__":
     main()
